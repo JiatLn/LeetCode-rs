608b471baa3c1dad595911bcbd2ebcc1dc545ea2
cx33 <lne128gene980@163.com>
2023-06-05 18:25:49 +0800

    chore: allow dead_code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+#![allow(dead_code)]
+
 mod maximum_tastiness_of_candy_basket; // No.2517
 mod count_vowel_strings_in_ranges; // No.2559
 mod swap_for_longest_repeated_character_substring; // No.1156
