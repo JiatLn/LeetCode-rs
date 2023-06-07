06ee95b6b6dc94897a88494cb786c6957254e367
cx33 <lne128gene980@163.com>
2023-06-07 22:41:32 +0800

    slove: 2611

--- src/main.rs
+++ src/main.rs
@@ -6,5 +6,6 @@ mod swap_for_longest_repeated_character_substring; // No.1156
 mod number_of_distinct_averages; // No.2465
 mod apply_operations_to_an_array; // No.2460
 mod equal_row_and_column_pairs; // No.2352
+mod mice_and_cheese; // No.2611
 
 fn main() {}
---
+++ src/mice_and_cheese/mod.rs
@@ -0,0 +1,43 @@
+/// <https://leetcode.cn/problems/mice-and-cheese/>
+struct Solution;
+
+impl Solution {
+    pub fn mice_and_cheese(reward1: Vec<i32>, reward2: Vec<i32>, k: i32) -> i32 {
+        let len = reward1.len();
+        let mut diffs = Vec::with_capacity(len);
+        for i in 0..len {
+            diffs.push([reward1[i] - reward2[i], reward1[i], reward2[i]]);
+        }
+        diffs.sort_by(|a, b| b[0].partial_cmp(&a[0]).unwrap());
+        let mut cnt = 0;
+        diffs.iter().fold(0, |acc, e| {
+            acc +
+                (if cnt < k {
+                    cnt += 1;
+                    e[1]
+                } else {
+                    e[2]
+                })
+        })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::mice_and_cheese::Solution;
+
+    #[test]
+    fn test_mice_and_cheese() {
+        let reward1 = vec![1, 1, 3, 4];
+        let reward2 = vec![4, 4, 1, 1];
+        let k = 2;
+        let ans = Solution::mice_and_cheese(reward1, reward2, k);
+        assert_eq!(ans, 15);
+
+        let reward1 = vec![1, 1];
+        let reward2 = vec![1, 1];
+        let k = 2;
+        let ans = Solution::mice_and_cheese(reward1, reward2, k);
+        assert_eq!(ans, 2);
+    }
+}
