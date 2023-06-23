7e6212a54ad98bbd8c6b7b078a32323e1c3b136a
cx33 <lne128gene980@163.com>
2023-06-23 17:51:09 +0800

    update: 2496

--- src/maximum_value_of_a_string_in_an_array/mod.rs
+++ src/maximum_value_of_a_string_in_an_array/mod.rs
@@ -4,7 +4,7 @@ struct Solution;
 impl Solution {
     pub fn maximum_value(strs: Vec<String>) -> i32 {
         strs.iter()
-            .map(|str| { str.parse::<i32>().unwrap_or(str.len() as i32) })
+            .map(|str| str.parse::<i32>().unwrap_or(str.len() as i32))
             .max()
             .unwrap()
     }
