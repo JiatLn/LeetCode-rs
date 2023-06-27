1d47bb682364daf4b520a5b748ef7031629a1e45
cx33 <lne128gene980@163.com>
2023-06-27 14:13:00 +0800

    chore: update script

--- index.ts
+++ index.ts
@@ -1,30 +1,24 @@
-import { mkdir, writeFile } from 'node:fs/promises';
+import { mkdir, writeFile, readFile } from 'node:fs/promises';
 import { argv, exit } from "node:process"
 
-if (argv.length < 3) {
-  console.error("缺少参数 [题目链接]")
-  exit(1)
+
+
+async function addQuestionTemplate(link: string, question: string) {
+  try {
+    await mkdir(`src/${question}`)
+    const path = `src/${question}/mod.rs`
+    await writeFile(path, rsTemplate(link, question), {
+      encoding: 'utf-8',
+      flag: 'w+'
+    })
+    console.log(`文件创建成功 ${path}`)
+  } catch (err) {
+    console.error(err.message);
+  }
 }
 
-const questionLink = argv[2]
-
-console.log('题目链接', questionLink)
-
-let questionName = questionLink.split("/").filter(Boolean).at(-1);
-
-try {
-  const fileName = questionName.replaceAll("-", "_")
-  await mkdir(`src/${fileName}`)
-  const file = `src/${fileName}/mod.rs`
-  await writeFile(file, rsTemplate(questionLink, fileName))
-  console.log(`文件创建成功 ${file}`)
-  console.log(`[TODO] 前往 src/main.rs 中添加 mod ${fileName};`)
-} catch (err) {
-  console.error(err.message);
-}
-
-function rsTemplate(questionLink: string, modName: string) {
-  return `/// <${questionLink}>
+function rsTemplate(link: string, modName: string) {
+  return `/// <${link}>
 struct Solution;
 
 impl Solution {
@@ -43,3 +37,48 @@ mod tests {
 
 `
 }
+
+async function addMainMod(question: string) {
+  try {
+    const originalContent = await readFile('src/main.rs', 'utf-8');
+    let lines = originalContent.split('\n');
+    lines.pop();
+    lines.pop();
+    lines.pop();
+    lines.push(`mod ${question};`, '', 'fn main() {}', '')
+    const modifiedContent = lines.join('\n');
+    await writeFile('src/main.rs', modifiedContent, {
+      encoding: 'utf-8',
+      flag: 'w+'
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+
+async function flow() {
+  if (argv.length < 3) {
+    console.error("缺少参数 [题目链接]")
+    exit(1)
+  }
+
+  const questionLink = argv[2]
+  console.log('题目链接', questionLink)
+
+  const question = questionLink
+    .split("/")
+    .filter(Boolean)
+    .at(-1)
+    .replaceAll("-", "_")
+
+  try {
+    await addQuestionTemplate(questionLink, question)
+    await addMainMod(question)
+  } catch (error) {
+    console.error('error', error)
+    exit(1)
+  }
+}
+
+flow()
