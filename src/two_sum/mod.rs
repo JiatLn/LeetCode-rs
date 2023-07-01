dd5050467fb9f39d9b51c3848e7640e7e4122939
cx33 <lne128gene980@163.com>
2023-07-01 11:02:49 +0800

    update: 1

--- src/two_sum/mod.rs
+++ src/two_sum/mod.rs
@@ -18,6 +18,26 @@ impl Solution {
         }
         nums
     }
+    pub fn two_sum_2(nums: Vec<i32>, target: i32) -> Vec<i32> {
+        let mut tuple_nums = nums.iter().enumerate().collect::<Vec<(_, _)>>();
+        tuple_nums.sort_by(|a, b| a.1.cmp(b.1));
+        let mut left = 0;
+        let mut right = tuple_nums.len() - 1;
+        while left < right {
+            match (tuple_nums[left].1 + tuple_nums[right].1).cmp(&target) {
+                std::cmp::Ordering::Less => {
+                    left += 1;
+                }
+                std::cmp::Ordering::Equal => {
+                    return vec![tuple_nums[left].0 as i32, tuple_nums[right].0 as i32];
+                }
+                std::cmp::Ordering::Greater => {
+                    right -= 1;
+                }
+            }
+        }
+        vec![]
+    }
 }
 
 #[cfg(test)]
@@ -32,6 +52,17 @@ mod tests {
 
         let nums = vec![3, 2, 4];
         let ans = Solution::two_sum(nums, 6);
+        assert_eq!(ans, vec![1, 2]);
+    }
+
+    #[test]
+    fn test_two_sum_2() {
+        let nums = vec![2, 7, 11, 15];
+        let ans = Solution::two_sum_2(nums, 9);
+        assert_eq!(ans, vec![0, 1]);
+
+        let nums = vec![3, 2, 4];
+        let ans = Solution::two_sum_2(nums, 6);
         assert_eq!(ans, vec![1, 2]);
     }
 }
