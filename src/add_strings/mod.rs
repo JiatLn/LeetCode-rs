2b6cb7d6e177c110e05e62dcf6ea538d0fd034fc
cx33 <lne128gene980@163.com>
2023-07-17 23:14:53 +0800

    update: 415

--- src/add_strings/mod.rs
+++ src/add_strings/mod.rs
@@ -3,26 +3,25 @@ struct Solution;
 
 impl Solution {
     pub fn add_strings(num1: String, num2: String) -> String {
-        let mut ans = vec![];
         let mut l1 = num1.len();
         let mut l2 = num2.len();
+        let mut ans = Vec::with_capacity(l1.max(l2) + 1);
         let mut num3 = 0;
-        while l1 > 0 || l2 > 0 || num3 > 0 {
+        while l1 > 0 || l2 > 0 {
             let mut val = num3;
             if l1 > 0 {
-                val += num1.as_bytes()[l1 - 1] - "0".as_bytes()[0];
+                val += num1.as_bytes()[l1 - 1] - b'0';
                 l1 -= 1;
             }
             if l2 > 0 {
-                val += num2.as_bytes()[l2 - 1] - "0".as_bytes()[0];
+                val += num2.as_bytes()[l2 - 1] - b'0';
                 l2 -= 1;
             }
-            if val > 9 {
-                num3 = 1;
-            } else {
-                num3 = 0;
-            }
+            num3 = if val > 9 { 1 } else { 0 };
             ans.push((val % 10).to_string());
+        }
+        if num3 > 0 {
+            ans.push(num3.to_string());
         }
         ans.reverse();
         ans.join("")
